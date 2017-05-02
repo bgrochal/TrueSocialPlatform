37eac7294f6ec411a4840217fdf90db7d96171ca
bgrochal <bartek.grochal@gmail.com>
2017-05-02 22:34:54 +0200

    Simple integration with Facebook login provider.

--- tsp-core/src/main/java/pl/edu/agh/iet/tsp/core/TrueSocialPlatform.java
+++ tsp-core/src/main/java/pl/edu/agh/iet/tsp/core/TrueSocialPlatform.java
@@ -2,11 +2,13 @@ package pl.edu.agh.iet.tsp.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
+@EnableOAuth2Sso
 public class TrueSocialPlatform {
 
     public static void main(String[] args) {
