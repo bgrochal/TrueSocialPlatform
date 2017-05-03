0f3dbe6377477b8bef80caefe9e333a94a2029e1
bgrochal <bartek.grochal@gmail.com>
2017-05-03 22:04:06 +0200

    Integration with Google as external login service provider added. Secret configuration externalized.

---
+++ tsp-login/src/main/java/pl/edu/agh/iet/tsp/login/oauth2/configuration/google/GoogleClientConfiguration.java
@@ -0,0 +1,44 @@
+package pl.edu.agh.iet.tsp.login.oauth2.configuration.google;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import pl.edu.agh.iet.tsp.login.oauth2.configuration.ClientConfiguration;
+
+/**
+ * @author Bartłomiej Grochal
+ */
+@Configuration
+public class GoogleClientConfiguration implements ClientConfiguration {
+
+    @Override
+    public AuthorizationCodeResourceDetails getClientProperties() {
+        return getGoogleClientProperties();
+    }
+
+    @Override
+    public ResourceServerProperties getResourceProperties() {
+        return getGoogleResourceProperties();
+    }
+
+    @Override
+    public String getLoginEndpoint() {
+        return "/login/google";
+    }
+
+
+    @Bean
+    @ConfigurationProperties("google.client")
+    public AuthorizationCodeResourceDetails getGoogleClientProperties() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    public ResourceServerProperties getGoogleResourceProperties() {
+        return new ResourceServerProperties();
+    }
+
+}
