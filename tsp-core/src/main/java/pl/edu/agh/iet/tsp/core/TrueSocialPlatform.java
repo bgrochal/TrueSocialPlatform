cf7f347859be5816edcdb2a84b2fddee6d0e107c
bgrochal <bartek.grochal@gmail.com>
2017-05-03 13:31:19 +0200

    More explicit way of handling login with an external provider.

--- tsp-core/src/main/java/pl/edu/agh/iet/tsp/core/TrueSocialPlatform.java
+++ tsp-core/src/main/java/pl/edu/agh/iet/tsp/core/TrueSocialPlatform.java
@@ -1,21 +1,59 @@
 package pl.edu.agh.iet.tsp.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.Filter;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class TrueSocialPlatform extends WebSecurityConfigurerAdapter {
+
+    @Autowired OAuth2ClientContext clientContext;
+
 
     public static void main(String[] args) {
         SpringApplication.run(TrueSocialPlatform.class, args);
+    }
+
+
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
     }
 
     @Override
@@ -23,7 +61,7 @@ public class TrueSocialPlatform extends WebSecurityConfigurerAdapter {
         http
             .antMatcher("/**")
             .authorizeRequests()
-                .antMatchers("/", "/login**")
+                .antMatchers("/", "/login/**")
                 .permitAll()
             .anyRequest()
                 .authenticated()
@@ -33,7 +71,22 @@ public class TrueSocialPlatform extends WebSecurityConfigurerAdapter {
                 .permitAll()
             .and()
                 .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter
+                = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), clientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        UserInfoTokenServices tokenServices =
+                new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+        tokenServices.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(tokenServices);
+        return facebookFilter;
     }
 
 }
