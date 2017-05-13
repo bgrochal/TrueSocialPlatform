291646b4b7de625a6ba18467577c00bdd0b30d90
bgrochal <bartek.grochal@gmail.com>
2017-05-13 19:51:28 +0200

    Definition of endpoint returning currently authenticated user moved to the proper controller.

--- tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/controller/UserController.java
+++ tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/controller/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.edu.agh.iet.tsp.web.controller.json.IdWrapper;
 import pl.edu.agh.iet.tsp.database.domain.User;
 import pl.edu.agh.iet.tsp.service.UserService;
 import pl.edu.agh.iet.tsp.service.exception.DuplicateUsernameException;
+import pl.edu.agh.iet.tsp.web.controller.json.IdWrapper;
+
+import java.security.Principal;
 
 /**
  * @author Wojciech Pachuta.
@@ -18,6 +20,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+    @RequestMapping("/me")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     public IdWrapper addUser(@RequestParam String username) throws DuplicateUsernameException {
