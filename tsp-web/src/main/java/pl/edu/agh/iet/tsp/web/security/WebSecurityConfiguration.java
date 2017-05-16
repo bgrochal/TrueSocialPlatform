6b69bf1d7c66dfd635de4b9f9f1abf76977ac764
bgrochal <bartek.grochal@gmail.com>
2017-05-16 16:14:51 +0200

    Redirection after successful login and retrieving data of currently logged in user implemented.

--- tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/security/WebSecurityConfiguration.java
+++ tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/security/WebSecurityConfiguration.java
@@ -5,27 +5,40 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoT
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.filter.CompositeFilter;
+import pl.edu.agh.iet.tsp.database.domain.AuthenticationData;
+import pl.edu.agh.iet.tsp.database.domain.User;
 import pl.edu.agh.iet.tsp.login.oauth2.config.ClientConfiguration;
+import pl.edu.agh.iet.tsp.login.oauth2.user.AuthenticatedUserDetails;
+import pl.edu.agh.iet.tsp.service.UserService;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
  * @author Bartłomiej Grochal
  */
 @Configuration
+@SuppressWarnings("SpringJavaAutowiringInspection")
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired OAuth2ClientContext clientContext;
-    @Autowired List<ClientConfiguration> clientConfigurations;
+    @Autowired private OAuth2ClientContext clientContext;
+    @Autowired private List<ClientConfiguration> clientConfigurations;
 
 
     @Override
@@ -60,6 +73,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private Filter ssoFilter(ClientConfiguration clientConfiguration) {
         OAuth2ClientAuthenticationProcessingFilter clientFilter
                 = new OAuth2ClientAuthenticationProcessingFilter(clientConfiguration.getLoginEndpoint());
+        clientFilter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandler(clientConfiguration));
         OAuth2RestTemplate clientTemplate =
                 new OAuth2RestTemplate(clientConfiguration.getClientProperties(), clientContext);
         clientFilter.setRestTemplate(clientTemplate);
@@ -71,6 +85,48 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         clientFilter.setTokenServices(tokenServices);
 
         return clientFilter;
+    }
+
+
+    // FIXME: The code below is not necessary in this class. Try to refactor.
+    @Autowired private UserService userService;
+    @Autowired private AuthenticatedUserDetails details;
+
+    private class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+        private final ClientConfiguration clientConfiguration;
+
+
+        AuthenticationSuccessHandler(ClientConfiguration clientConfiguration) {
+            this.clientConfiguration = clientConfiguration;
+        }
+
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                            Authentication authentication) throws IOException, ServletException {
+            Map<String, Object> authenticatedUserDetails =
+                    (Map<String, Object>) ((OAuth2Authentication) authentication).getUserAuthentication().getDetails();
+            AuthenticationData.AuthenticationProvider authenticationProvider =
+                    clientConfiguration.getAuthenticationProvider();
+            AuthenticationData authenticationData = new AuthenticationData(authenticationProvider,
+                    authenticationProvider.getOAuthUniqueIdentifier(authenticatedUserDetails));
+
+            Optional<User> domainUser = userService.getUserByAuthenticationData(authenticationData);
+            if (domainUser.isPresent()) {
+                setDefaultTargetUrl("/user");
+                details.setDomainUser(domainUser.get());
+            } else {
+                User newUser = new User(null, authenticationData);
+                setDefaultTargetUrl("/user/new");
+                details.setDomainUser(newUser);
+            }
+            details.setFullName(authenticationProvider.getOAuthFullName(authenticatedUserDetails));
+
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
+
     }
 
 }
