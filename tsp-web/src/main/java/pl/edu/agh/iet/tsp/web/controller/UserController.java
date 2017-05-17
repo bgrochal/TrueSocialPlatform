df0eb2de6d8812d184d781c34edd7aaa0fca6ad4
bgrochal <bartek.grochal@gmail.com>
2017-05-17 02:43:54 +0200

    Controller handling user-related requests implemented.

--- tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/controller/UserController.java
+++ tsp-web/src/main/java/pl/edu/agh/iet/tsp/web/controller/UserController.java
@@ -1,22 +1,18 @@
 package pl.edu.agh.iet.tsp.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.iet.tsp.database.domain.User;
+import pl.edu.agh.iet.tsp.login.oauth2.user.AuthenticatedUserDetails;
 import pl.edu.agh.iet.tsp.service.UserService;
 import pl.edu.agh.iet.tsp.service.exception.DuplicateUsernameException;
 import pl.edu.agh.iet.tsp.web.controller.json.IdWrapper;
 
-import java.security.Principal;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+
 
 /**
  * @author Bartłomiej Grochal
@@ -26,51 +22,36 @@ import java.util.Optional;
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    @Autowired private UserService userService;
+    @Autowired private AuthenticatedUserDetails authenticatedUserDetails;
 
 
     @RequestMapping("/me")
-    public ResponseEntity user(Principal principal) {
-        Map<String, String> userInfo = new HashMap<>();
-        Map<String, String> authenticatedUserDetails =
-                (Map<String, String>) ((OAuth2Authentication) principal).getUserAuthentication().getDetails();
+    public HashMap<String, String> currentUser() {
+        HashMap<String, String> currentUserDetails = new HashMap<>();
+        currentUserDetails.put("name", authenticatedUserDetails.getFullName());
+        currentUserDetails.put("username", authenticatedUserDetails.getDomainUser().getUsername());
 
-        userInfo.put("name", authenticatedUserDetails.get("name"));
-        userInfo.put("email", authenticatedUserDetails.get("email"));
-
-        if (userInfo.get("email") != null) {
-            try {
-                handleAuthenticatedUser(userInfo.get("email"));
-            } catch (DuplicateUsernameException e) {
-                userInfo.put("errorMessage", "Unexpected error occurred while creating database entry for given user.");
-                return ResponseEntity
-                        .status(HttpStatus.CONFLICT)
-                        .body(userInfo);
-            }
-            return ResponseEntity.ok(userInfo);
-        }
-
-        userInfo.put("errorMessage", "The login request cannot be completed because the email address is unavailable.");
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(userInfo);
+        return currentUserDetails;
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     public IdWrapper addUser(@RequestParam String username) throws DuplicateUsernameException {
-        User user = new User(username);
+        User user = new User(username, authenticatedUserDetails.getDomainUser().getAuthenticationData());
+        userService.addUser(user);
+
+        authenticatedUserDetails.setDomainUser(user);
         return new IdWrapper(userService.addUser(user));
     }
 
+    @RequestMapping(value = "/user/new", method = RequestMethod.GET)
+    public String showNewUser() {
+        return "forward:/";
+    }
 
-    private IdWrapper handleAuthenticatedUser(String userName) throws DuplicateUsernameException {
-        Optional<User> user = userService.getUser(userName);
-        if (!user.isPresent()) {
-            User newUser = new User(userName);
-            return new IdWrapper(userService.addUser(newUser));
-        }
-        return new IdWrapper(user.get().getId());
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String showUser() {
+        return "forward:/";
     }
 
 }
