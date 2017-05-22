800e02fa29afdc9e93b06f2d01e3480a47e47ab8
bgrochal <bartek.grochal@gmail.com>
2017-05-23 00:02:09 +0200

    View for user's posts implemented.

--- tsp-web/src/main/resources/static/js/controller/userController.js
+++ tsp-web/src/main/resources/static/js/controller/userController.js
@@ -9,9 +9,17 @@ app.controller('userController', ['$scope', '$http', function ($scope, $http) {
         }
     };
 
-    $scope.newPostSubmit = function () {
+    $scope.clearAlerts = function () {
         $scope.newPostError = null;
         $scope.newPostSuccess = null;
+
+        $scope.latestPostsError = null;
+        $scope.commentsToPostError = null;
+        $scope.editedPostError = null;
+    };
+
+    $scope.newPostSubmit = function () {
+        $scope.clearAlerts();
 
         $http({
             url     : '/posts',
@@ -30,7 +38,120 @@ app.controller('userController', ['$scope', '$http', function ($scope, $http) {
             }
         );
 
-        $('form[name="newPostForm"]').reset();
+        $('form[name="newPostForm"]')[0].reset();
+    };
+
+    $scope.getLatestPosts = function () {
+        $scope.latestPostsError = null;
+        $scope.hasMorePosts = true;
+
+        $http({
+            url     : '/posts/latest',
+            method  : 'GET',
+            params  : {
+                'number' : 5
+            }
+        }).then(
+            function (response) {
+                $scope.allPosts = response.data.data;
+                $scope.nextPageUrl = response.data.next;
+                $scope.hasMorePosts = $scope.nextPageUrl != null;
+            },
+            function (error) {
+                $scope.latestPostsError = 'Unexpected error: ' + error.status + '.';
+                console.log(error);
+            }
+        );
+    };
+
+    $scope.getMorePosts = function () {
+        $http({
+            url     : $scope.nextPageUrl,
+            method  : 'GET'
+        }).then(
+            function (response) {
+                $scope.allPosts = $scope.allPosts.concat(response.data.data);
+                $scope.nextPageUrl = response.data.next;
+                $scope.hasMorePosts = $scope.nextPageUrl != null;
+            },
+            function (error) {
+                $scope.latestPostsError = 'Unexpected error: ' + error.status + '.';
+                console.log(error);
+            }
+        );
+    };
+
+    $scope.deletePost = function (postId) {
+        // TODO: make a request to the server and put the code below into the successful callback.
+        $scope.allPosts = $scope.allPosts.filter(function (post) {
+            return post.id != postId;
+        });
+    };
+
+    $scope.editPost = function (post) {
+        $scope.editedPost = $.extend(true, {}, post);
+        $('#editPostModal').modal();
+    };
+
+    $scope.editPostSubmit = function () {
+        $http({
+            url     : '/posts/' + $scope.editedPost.id,
+            method  : 'PUT',
+            params  : {
+                'authorId' : $scope.editedPost.authorId
+            },
+            data    : $scope.editedPost
+        }).then(
+            function (response) {
+                $scope.allPosts.forEach(function (post, i) {
+                    if (post.id == $scope.editedPost.id) {
+                        $scope.allPosts[i] = $scope.editedPost;
+                    }
+                });
+
+                $('#editPostModal').modal('hide');
+            },
+            function (error) {
+                $scope.editedPostError = 'Unexpected error: ' + error.status + '.';
+                console.log(error);
+            }
+        );
+    };
+
+    $scope.getCommentsToPost = function (post) {
+        $http({
+            url     : '/comments/latest',
+            method  : 'GET',
+            params  : {
+                'postId' : post.id,
+                'number' : 5
+            }
+        }).then(
+            function (response) {
+                post.comments = response.data.data;
+                post.nextComments = response.data.next;
+            },
+            function (error) {
+                $scope.commentsToPostError = 'Unexpected error: ' + error.status + '.';
+                console.log(error);
+            }
+        );
+    };
+
+    $scope.getMoreCommentsToPost = function (post) {
+        $http({
+            url     : post.nextComments,
+            method  : 'GET'
+        }).then(
+            function (response) {
+                post.comments = post.comments.concat(response.data.data);
+                post.nextComments = response.data.next;
+            },
+            function (error) {
+                $scope.commentsToPostError = 'Unexpected error: ' + error.status + '.';
+                console.log(error);
+            }
+        );
     };
 
     $scope.init();
